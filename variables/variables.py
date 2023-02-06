94ed9d6fdc11c026e99d37e2c10670d8475c7b5e
nickramen <katherine.nicoleram@gmail.com>
2023-02-06 17:41:56 -0600

    Added folders with python basic examples

---
+++ variables/variables.py
@@ -0,0 +1,29 @@
+a = 15
+b = 5
+c = a + b
+print(c)
+
+
+name = "katherine"
+lastname = "nicole"
+print(name)
+
+c += 1
+c -= 1
+print(c)
+
+
+full_name = name + lastname
+welcome = f"Hello {full_name}, how are you?" #fstrings
+
+# operadores de pertenencia (in y not in)
+
+print("Nicole" in welcome) #true
+print("Nicole" not in welcome) #false
+
+
+# camelCase
+unaVariable = "variable1"
+
+# snake_case
+una_variable = "variable2"
